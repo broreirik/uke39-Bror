4638bc4bc8b2aca390a23cca487836fb9e47416e
broreirik <broreirikmac@icloud.com>
2026-09-24 09:11:57 +0200

    Ferdig med testing

--- test/script.js
+++ test/script.js
@@ -1,1 +1,7 @@
+console.log("Hello, World!");
 
+let navn = "Bror";
+let alder = 16;
+let bosted = "Løten";
+
+console.log("Hei, jeg heter " + navn + ", jeg er " + alder + " år gammel og bor i " + bosted + ".");
